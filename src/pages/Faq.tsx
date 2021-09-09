6fb349931ba5db5cf69c3829d38490b0fd62ffef
Tony Trinh (tr1nni) <trinhx@gmail.com>
2021-09-09 23:53:19 +1000

    updated faq

--- src/pages/Faq.tsx
+++ src/pages/Faq.tsx
@@ -4,24 +4,27 @@ function Faq() {
 			<h1 className="text-2xl mb-4">Frequently Asked Questions</h1>
 			<h2 className="text-xl text-indigo-200 mt-2">What is an NFT?</h2>
 			<p className="leading-relaxed mb-8 text-gray-400">
-				NFT stands for "non-fungible token." An NFT is basically data that is stored or accounted for in a digital ledger, and that data represents something specific. An NFT can, for example,
-				represent a piece of art, a music album or other types of digital files.
+                An NFT is a  "non-fungible token." An NFT is data that is stored or accounted for in a digital ledger, and that data represents something specific. An NFT can, for example, represent a piece of art, a music album or other types of digital files.
 			</p>
 
 			<h2 className="text-xl text-indigo-200 mt-2">Where can I buy or sell PizzSol?</h2>
-			<p className="leading-relaxed mb-8 text-gray-400">We're currently listed on Solsea. </p>
+			<p className="leading-relaxed mb-8 text-gray-400">
+                We are currently awaiting verification confirmation from DigitalEyes, Solanart and Solsea.
+            </p>
 
 			<h2 className="text-xl text-indigo-200 mt-2">Where can I view my PizzSol?</h2>
-			<p className="leading-relaxed mb-8 text-gray-400">We recommend Phantom wallet or Sollet. Click on the NFT tab in either wallet and you'll see your PizzSols!</p>
+			<p className="leading-relaxed mb-8 text-gray-400">
+                Phantom wallet can be used to view your NFT. You can also connect most other Solana wallets to step.finance to view your NFT.
+            </p>
 
-			<h2 className="text-xl text-indigo-200 mt-2">What can I do with my PizzSol NFT?</h2>
+			<h2 className="text-xl text-indigo-200 mt-2">
+                You are free to do anything with your PizzSols under a non-exclusive license.
+            </h2>
 			<p className="leading-relaxed mb-8 text-gray-400">You are free to do anything with your PizzSols under a non-exclusive license.</p>
 
 			<h2 className="text-xl text-indigo-200 mt-2">How were the PizzSols generated?</h2>
 			<p className="leading-relaxed mb-8 text-gray-400">
-				All individual pieces of the PizzSol world have been created by hand and algorithmically generated using custom code. There are 165 attributes across 5 categories: These categories include
-				background, pizza base, protein, cheese topping, sauce, vegetable. With these combined, it gives a total mathematical combination of thousands of unique PizzSols - but with only 10,000 being
-				minted, some are more unique and rare than others.
+                A PizzSol is algorithmically generated using a custom code. There are 54 attributes across 5 categories: These categories include pizza bases, meat toppings, vegetable toppings, a variety of cheeses and sauces. With these combined, it gives a total mathematical combination of nearly 100,000 unique PizzSols - but with only 10,000 being minted, some are more unique and rare than others; in particular the very rare PizzSol sauce topping.
 			</p>
 
 			<h2 className="text-xl text-indigo-200 mt-2">Get in touch with us</h2>
