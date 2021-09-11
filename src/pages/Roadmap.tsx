810b74d4738bda55d37609e916783396ec13a7b2
Tony Trinh (tr1nni) <trinhx@gmail.com>
2021-09-11 11:08:27 +1000

    updated front content and roadmap fixed typos in attributes

--- src/pages/Roadmap.tsx
+++ src/pages/Roadmap.tsx
@@ -6,20 +6,23 @@ function Team() {
 					<div className="p-4 font-dejavu text-sm sm:text-base">
 						<h1 className="text-base sm:text-lg mb-4">Roadmap</h1>
 
-						<p style={{ color: 'lime' }}>September:</p>
+						<p style={{ color: 'lime' }}>September 2021:</p>
 						<ul>
 							<li>10,000 Pizzsols Released!</li>
 						</ul>
 						<br></br>
-						<p style={{ color: 'lime' }}>October:</p>
+						<p style={{ color: 'lime' }}>October 2021:</p>
 						<p className="mb-2">Airdrops for Pizzsol holders with special combinations of ingredients that make up popular pizzas around the world.</p>
 						<p>Seek out collaborations and partnerships with the wider NFT ecosystem</p>
 						<br></br>
-						<p style={{ color: 'lime' }}>November:</p>
+						<p style={{ color: 'lime' }}>November 2021:</p>
 						<p>Voting and ranking system for favourite Pizzsol recipes</p>
 						<br></br>
-						<p style={{ color: 'lime' }}>December:</p>
+						<p style={{ color: 'lime' }}>December 2021:</p>
 						<p>There will be an ultra rare PizzSol Special Sauce which Community driven and created.</p>
+                        <br></br>
+						<p style={{ color: 'lime' }}>January 2022:</p>
+                        <p>Create your own PizzSol!</p>
 					</div>
 				</div>
 			</div>
