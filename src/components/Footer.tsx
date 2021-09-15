4cb1d49b36f10a9fe7c3e2fc98013a10269cda47
Tony Trinh (tr1nni) <trinhx@gmail.com>
2021-09-15 20:20:27 +1000

    playing around with new front layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,12 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
 	return (
 		<footer style={{ backgroundColor: '#151515' }} className="p-6 m-4 text-center text-gray-500 flex justify-center gap-6">
+            <div className="flex flex-col">
 			<Link to={'/terms'}>
 				<p className="hover:text-white">Terms and Conditions</p>
 			</Link>
+            <span className="text-sm p-2">The eatPizzSol team. All rights reserved</span>
+            </div>
 		</footer>
 	);
 };
