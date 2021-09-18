0485ca79d02a9296e2be5babce4d008ff499fe83
Tony Trinh (tr1nni) <trinhx@gmail.com>
2021-09-18 11:18:28 +1000

    new landing page more content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
-// import pizzsolGifFast from '../assets/pizzsols_fast_2.gif';
+import pizzsolGifFast from '../assets/pizzsols_fast_2.gif';
+import licences from '../assets/licences.gif';
 import rarePizzSols from '../assets/rare_pizzsols.gif';
 
 import { Links } from '../constants/Links';
@@ -6,25 +7,54 @@ import { Links } from '../constants/Links';
 function Home() {
 	return (
 		<div>
-			<div className="max-w-3xl mx-auto px-2 sm:px-4">
-				<div className="text-gray-300 leading-relaxed">
-					The team here
-					<a href={Links.TWITTER} target="_blank" rel="noreferrer" className="hover:text-indigo-700">
-						<span>{` @eatPizzSol `}</span>
-					</a>
-					love pizza. We want to ensure your favourite recipes exist on the blockchain forever.
-					<h1 className="text-2xl font-semibold text-center mt-10 mb-10">Pizzsols will bridge human senses to the metaverse via the world's universal food... Pizza!</h1>
-					PizzSol is randomly generated pizza ingredients on the Solana blockchain. Images and Stats are intentionally ommited allowing others to interpret and use in anyway they want. Each PizzSol is
-					comprised of a unique base, meats, vegetables, cheeses and sauces. For more information on our roadmap, FAQ etc, join our
-					<a href={Links.DISCORD} target="_blank" rel="noreferrer" className="hover:text-gray-100">
-						<span>{` Discord!`}</span>
-					</a>
-					<br />
+			<div className="max-w-3xl mx-auto px-2 sm:px-4 px-4">
+
+				<div className="block px-2 sm:p-4 mx-auto mt-8">
+					<img src={pizzsolGifFast} alt="PizzSols!" />
 				</div>
 
-				<div className="flex flex-wrap mx-auto justify-center">
-					<span className="bg-indigo-500 hover:bg-indigo-700 text-white text-center py-2 px-4 rounded mt-8 mb-16">Minting Soon!</span>
+				<div className="block sm:p-4 mx-auto h-100 p-5">
+					<h1 className="text-2xl font-medium text-left mt-5 mb-3">
+						THE FIRST 10,000 ALGORITHMICALLY GENERATED PIZZA RECIPES ON SOLANA
+					</h1>
+					<span className="text-gray-300 leading-relaxed font-light text-lg p-5">
+						The team here
+						<a href={Links.TWITTER} target="_blank" rel="noreferrer" className="hover:text-indigo-700">
+							<span>{` @eatPizzSol `}</span>
+						</a>
+						love pizza we want to deliver pizzas into the metaverse the way you love them.
+						PizzSols are NFTs with algorithmically generated pizza ingredients on the Solana blockchain. Images and Stats
+                        are intentionally ommited allowing others to interpret and use in anyway they want. Each PizzSol is
+                        comprised of a unique base, meats, vegetables, cheeses and sauces.
+					</span>
 				</div>
+
+				<div className="block sm:p-4 mx-auto h-50 w-full py-20 px-5">
+					<h1 className="text-2xl font-medium text-left mt-5 mb-3">
+						CREATORS TO CREATE PIZZAS THE WAY THEY WANT
+					</h1>
+					<span className="text-gray-300 leading-relaxed font-light text-lg p-5">
+						The metaverse will be vast and every delivery is an
+						adventure. We need to get these PizzSols out, and it won't be easy. Expert drivers will be required to
+						operate class A, B, C, D vehicles.
+					</span>
+				</div>
+
+				<div className="block sm:p-4 mx-auto h-50 w-full bg-pink-700 p-5">
+					<h1 className="text-2xl text-right font-medium mt-5 p-5">
+						... SO WE NEED YOUR HELP
+					</h1>
+
+					<span className="text-gray-300 leading-relaxed font-light text-lg mt-3">
+						Delivering PizzSols across the metaverse is hardwork. We're looking for drivers. The owners
+						of the FIRST 500 PizzSols #00001 - #00500 will be airdropped an official PizzSol drivers licence.
+					</span>
+
+					<div className="block px-2 sm:p-4 mx-auto mt-8">
+						<img src={licences} alt="PizzSols!" />
+					</div>
+				</div>
+
 			</div>
 
 			<div className="bg-indigo-800 leading-relaxed w-full mb-24">
@@ -52,7 +82,12 @@ function Home() {
 						<img src={rarePizzSols} alt="Ultra-Rare PizzSols!" />
 					</div>
 				</div>
+
+
 			</div>
+				<div className="flex flex-wrap mx-auto justify-center">
+					<span className="bg-indigo-500 hover:bg-indigo-700 text-white text-center py-2 px-4 rounded mt-8 mb-16">Minting Soon!</span>
+				</div>
 		</div>
 	);
 }
