59097218a110100935525c2114e56aa7225785b9
Tony Trinh (tr1nni) <trinhx@gmail.com>
2021-09-19 13:20:37 +1000

    updated roadmap

--- src/pages/Roadmap.tsx
+++ src/pages/Roadmap.tsx
@@ -4,15 +4,16 @@ function Team() {
 			<div className="flex flex-wrap mx-auto justify-center">
 				<div style={{ width: '512px' }} className="mx-4 mb-10 bg-black pb-8">
 					<div className="p-4 font-dejavu text-sm sm:text-base">
-						<h1 className="text-base sm:text-lg mb-4">Roadmap</h1>
+						<h1 className="text-base sm:text-xl mb-4 text-center font-bold">ROADMAP</h1>
 
-						<p style={{ color: 'lime' }}>September 2021:</p>
+						<p style={{ color: 'lime' }}>September 26th 3pm UTC 2021:</p>
 						<ul>
-							<li>10,000 Pizzsols Released!</li>
+							<li>FIRST and ONLY 10,000 Pizzsols Released!</li>
 						</ul>
 						<br></br>
 						<p style={{ color: 'lime' }}>October 2021:</p>
-						<p className="mb-2">Airdrops for Pizzsol holders with special combinations of ingredients that make up popular pizzas around the world.</p>
+						<p className="mb-2">In search for PizzSol delivery drivers. Airdropping PizzSol drivers licences to
+                        holders with particular ingredients. Join our discord for clues.</p>
 						<p>Seek out collaborations and partnerships with the wider NFT ecosystem</p>
 						<br></br>
 						<p style={{ color: 'lime' }}>November 2021:</p>
