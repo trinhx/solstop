573db514dea2b684f1a92d15430e23f9b021bd6b
Tony <trinhx@gmail.com>
2021-09-25 20:17:19 +1000

    updated roadmap

--- src/pages/Roadmap.tsx
+++ src/pages/Roadmap.tsx
@@ -13,32 +13,59 @@ function Team() {
 						<p style={{ color: 'lime' }}>September 26th 3pm UTC 2021:</p>
 						<ul>
 							<li>FIRST and ONLY 10,000 Pizzsols Released!</li>
+                            <li>All unique combinations of your favourite Pizza recipes.</li>
+                            <li>Only 7 Ultra-Rare PizzSols with an emoji.</li>
 						</ul>
+					</div>
+				</div>
 
-						<br></br>
+				<div style={{ width: '512px' }} className="mx-4 mb-10 bg-black pb-8">
+					<div className="p-4 font-dejavu text-sm sm:text-base">
 						<p style={{ color: 'lime' }}>October 2021:</p>
 						<p className="mb-2">
-							In search for PizzSol delivery drivers. Airdropping PizzSol drivers licences to holders with particular ingredients.
-							<br />
-							<br />
+							The search for PizzSol delivery drivers begins. We want to airdrop special licences to holders with particular ingredients to
+                            become our future drivers.
+							<br/>
+							<br/>
 							Join our{' '}
 							<a href={Links.DISCORD} target="_blank" rel="noreferrer" className="hover:text-indigo-700">
 								Discord
 							</a>{' '}
 							for clues!
 						</p>
-						<br></br>
-						<p style={{ color: 'lime' }}>November 2021:</p>
-						<p>Seek out collaborations and partnerships with the wider NFT ecosystem</p>
-						<br></br>
-						<p style={{ color: 'lime' }}>December 2021:</p>
-                        <p>The biggest secret of all.</p>
-						<p>There will be an ultra rare PizzSol Special Sauce which Community driven and created.</p>
-						<br></br>
-						<p style={{ color: 'lime' }}>January 2022:</p>
-						<p>Create your own PizzSol!</p>
 					</div>
 				</div>
+
+				<div style={{ width: '512px' }} className="mx-4 mb-10 bg-black pb-8">
+					<div className="p-4 font-dejavu text-sm sm:text-base">
+						<p style={{ color: 'lime' }}>November 2021:</p>
+                        <p>We want everyone to try a slice of PizzSol.</p>
+						<p>Seek out collaborations and partnerships with the wider NFT ecosystem.</p>
+                        <br/>
+						<p>Prototyping of PizzSols incorporated into some popular games.</p>
+                        <br/>
+                        <p>Engage with effective charities</p>
+					</div>
+				</div>
+
+				<div style={{ width: '512px' }} className="mx-4 mb-10 bg-black pb-8">
+					<div className="p-4 font-dejavu text-sm sm:text-base">
+						<p style={{ color: 'lime' }}>December 2021:</p>
+                        <p>One of the biggest secrets of all. Clues will be dropped about this special recipe.</p>
+                        <br/>
+						<p>There will be an ultra rare PizzSol Special Sauce which Community driven and created.</p>
+					</div>
+				</div>
+
+				<div style={{ width: '512px' }} className="mx-4 mb-10 bg-black pb-8">
+					<div className="p-4 font-dejavu text-sm sm:text-base">
+						<p style={{ color: 'lime' }}>January 2022:</p>
+						<p>Create and mint your own PizzSol!</p>
+                        <br/>
+						<p>Merge your personal PizzSol into game</p>
+					</div>
+				</div>
+
 			</div>
 		</div>
 	);
